fbe1c8cd4b0205b3ca31bc91596c803c36b8d504
Max Hanson <mthanson2@gmail.com>
2020-11-06 16:38:13 -0800

    Created api calls

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
+const randomEmployeeUrl = "https://randomuser.me/api/?results=200&nat=us";
 
 export default {
   getRandomEmployees: function() {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
+    return axios.get(randomEmployeeUrl);
   }
 };
-
-// https://randomuser.me/api/?results=200&nat=us

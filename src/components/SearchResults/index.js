e7f7636ac34cc7edd2870b5e648608b5ddfd7c84
Max Hanson <mthanson2@gmail.com>
2020-11-06 16:18:43 -0800

    Added compnents and add somethings to app.js

---
+++ src/components/SearchResults/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+function SearchResults(props) {
+  return (
+    <ul className="list-group search-results">
+      {props.results.map(result => (
+        <li key={result} className="list-group-item">
+          <img alt="Employees" src={result.picture.thumbnail} className="img-fluid" />
+          <p>{result.name}</p>
+          <p>{result.phone}</p>
+          <p>{result.email}</p>
+          <p>{result.dob.age}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default SearchResults;
